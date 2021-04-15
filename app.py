0ba10488af1ef23581eaf479e524b931e47358c0
felender <felender7@gmail.com>
2021-04-15 13:06:01 +0200

    Implementing the script to write on the host file

--- app.py
+++ app.py
@@ -14,8 +14,10 @@ def machine_type():
         #hosts_path="/etc/hosts"
         hosts_path_tem = "hosts"
     else:
-        hosts_path=r"c:\Windows\System32\drivers\etc\hosts"
-    return
+        #hosts_path =r"c:\Windows\System32\drivers\etc\hosts"
+        hosts_path_tem =r"c:\Windows\System32\drivers\etc\hosts"
+
+    return hosts_path_tem
 
 # initialise redirect varaible and list of blocked website
 redirect="127.0.0.1"
@@ -24,9 +26,20 @@ website_list=["www.facebook.com","facebook.com","gmail.com", "www.gmail.com"]
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working Hours...")
-        with open(machine_type(), 'r+') as file:
-            content.file.read()
-            print(content)
+
+        #Get host path and open/read
+        path = machine_type()
+        with open(path, 'r+') as file:
+            content = file.read()
+        # check if the content exist on the host file
+        # if the content exist then pass else save the content
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+
+
     else:
         print("Fun Hours...")
     time.sleep(5)
