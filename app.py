6fa955309c73222bbf2f64845aa0f0c1de752d89
felender <felender7@gmail.com>
2021-04-15 12:33:20 +0200

    Setting up the Infinite Loop

--- app.py
+++ app.py
@@ -12,10 +12,20 @@ def machine_type():
     h_path = platform.system()
     if h_path == "Darwin":
         #hosts_path="/etc/hosts"
+        hosts_path_tem = "hosts"
     else:
         hosts_path=r"c:\Windows\System32\drivers\etc\hosts"
     return
 
+# initialise redirect varaible and list of blocked website
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","gmail.com", "www.gmail.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working Hours...")
+    else:
+        print("Fun Hours...")
+    time.sleep(5)
+
 machine_type()
