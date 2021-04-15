8ce792eec809256f04818ee797e304d75c065f04
felender <felender7@gmail.com>
2021-04-15 12:50:37 +0200

    Setting up the Infinite Loop

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ website_list=["www.facebook.com","facebook.com","gmail.com", "www.gmail.com"]
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working Hours...")
+        with open(machine_type(), 'r+') as file:
+            content.file.read()
+            print(content)
     else:
         print("Fun Hours...")
     time.sleep(5)
